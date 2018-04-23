5503c9855dfa3c7523c340290557229ffffae739
李肇男 <32943976+LIZHAONAN@users.noreply.github.com>
2018-04-23 19:59:58 -0400

    Add Player class

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+  String name;
+  String nationality;
+  int age;
+
+  public Player(String n, String na, int a){
+    this.name = n;
+    this.nationality = na;
+    this.age = a;
+  }
+}
