6cc3292654df484edd1d9cc2612d9e4afde4212d
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 17:45:47 -0400

    Update Rankings.java

--- Rankings.java
+++ Rankings.java
@@ -10,50 +10,34 @@ public class Rankings extends JPanel{
 
   //Use treemaps to store map phone numbers to names, and the reverse
 
-  public static JLabel firstPlayer = new JLabel("");
-  public static JLabel secondPlayer = new JLabel("");
-  public static JLabel thirdPlayer = new JLabel("");
-  public static JLabel fourthPlayer = new JLabel("");
-  public static JLabel fifthPlayer = new JLabel("");
+  JPanel content;
+  int count = 1;
 
 
   public Rankings(){
 
     //Create a graphical user interface
     super();
-    JPanel content = this;
+    content = this;
 
-    JLabel placeOne = new JLabel("1. ");
-    if(Competition.players.size()>0){
-      firstPlayer.setText(Competition.players.get(0).name);
-    }
-    JLabel placeTwo = new JLabel("2. ");
-    if(Competition.players.size()>1){
-      secondPlayer.setText(Competition.players.get(1).name);
-    }
-    JLabel placeThree = new JLabel("3. ");
-    if(Competition.players.size()>2){
-      thirdPlayer.setText(Competition.players.get(2).name);
-    }
-    JLabel placeFour = new JLabel("4. ");
-    if(Competition.players.size()>3){
-      fourthPlayer.setText(Competition.players.get(3).name);
-    }
-    JLabel placeFive = new JLabel("5. ");
-    if(Competition.players.size()>4){
-      fifthPlayer.setText(Competition.players.get(4).name);
-    }
 
     content.setLayout(new GridLayout(5,2));
-    content.add(placeOne);
-    content.add(firstPlayer);
-    content.add(placeTwo);
-    content.add(secondPlayer);
-    content.add(placeThree);
-    content.add(thirdPlayer);
-    content.add(placeFour);
-    content.add(fourthPlayer);
-    content.add(placeFive);
-    content.add(fifthPlayer);
+    content.setVisible(true);
+  }
+
+  public void addPlayer(String name){
+    JLabel label = new JLabel(count+".");
+    count++;
+    JButton nameLabel = new JButton(name);
+    //nameLabel.addActionListener(new ActionListener() {
+		//	public void actionPerformed(ActionEvent event) {
+    //    String s =
+    //    JOptionPane.setMessageDialog(null, )
+  //    }
+  //  }
+    content.add(label);
+    content.add(nameLabel);
+    content.revalidate();
+    content.repaint();
   }
 }
