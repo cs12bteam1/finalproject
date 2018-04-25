2421771ccda07e4033689e96c3b65d4faa0d5574
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 16:27:28 -0400

    Update AddPlayer.java

--- AddPlayer.java
+++ AddPlayer.java
@@ -1,16 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class AddPlayer extends JPanel{
-
-  ArrayList<Player> players;
 
   public AddPlayer(){
     super();
     JPanel content = this;
-    players = new ArrayList<Player>();
+
 
     JLabel header = new JLabel("ADD PLAYERS!");
     content.add(header, BorderLayout.PAGE_START);
@@ -20,11 +18,11 @@ public class AddPlayer extends JPanel{
     JTextField name = new JTextField("Add name");
     JTextField nationality = new JTextField("Add nationality");
     JTextField age = new JTextField("Add age");
+    JTextField rating = new JTextField("Add rating");
     input.add(name);
     input.add(nationality);
     input.add(age);
-
-    JOptionPane warning = new JOptionPane();
+    input.add(rating);
 
     JButton submit = new JButton("Submit");
     submit.addActionListener(new ActionListener(){
@@ -32,25 +30,29 @@ public class AddPlayer extends JPanel{
         String n = name.getText();
         String na = nationality.getText();
         String a = age.getText();
-        if(!isInteger(a)){
-          warning.showMessageDialog(content,
-            "Failed to submit: Please enter an integer for age ",
-            "Inane error",
-            JOptionPane.ERROR_MESSAGE);
+        String ra = rating.getText();
+        Competition.players.add(new Player(n,na,Integer.parseInt(a), Integer.parseInt(ra)));
+        Competition.playerChooser.addItem(n);
+        Competition.playerChooser2.addItem(n);
+        Collections.sort(Competition.players);
+        Rankings.firstPlayer.setText(Competition.players.get(0).name);
+        if(Competition.players.size()>1){
+          Rankings.secondPlayer.setText(Competition.players.get(1).name);
         }
-        else players.add(new Player(n,na,a));
+        if(Competition.players.size()>2){
+          Rankings.thirdPlayer.setText(Competition.players.get(2).name);
+        }
+        if(Competition.players.size()>3){
+          Rankings.fourthPlayer.setText(Competition.players.get(3).name);
+        }
+        if(Competition.players.size()>4){
+          Rankings.fifthPlayer.setText(Competition.players.get(4).name);
+        }
       }
     });
 
     content.add(input, BorderLayout.CENTER);
     content.add(submit, BorderLayout.PAGE_END);
-  }
-
-  public static boolean isInteger(String s){
-    for (char c : s.toCharArray()){
-      if(!Character.isDigit(c)) return false;
-    }
-    return true;
   }
 
 }
