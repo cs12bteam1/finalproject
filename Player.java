d0b8fde642a27597e55c02513285b77e86fb01d0
Osamah Mandawi <32464574+Mandawi@users.noreply.github.com>
2018-04-25 16:32:07 -0400

    player points -> double

--- Player.java
+++ Player.java
@@ -5,7 +5,7 @@ public class Player implements Comparator<Player>{
   String nationality;
   int age;
   int rating;
-  int points;
+  double points;
 
   public Player(String n, String na, int a, int rating){
     this.name = n;
