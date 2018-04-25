4578b90da2e48aac87b9e49ce6acfa49296df85e
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 15:59:31 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,6 @@
-public class Player{
+import java.util.*;
+
+public class Player implements Comparator<Player>{
   String name;
   String nationality;
   int age;
@@ -11,5 +13,22 @@ public class Player{
     this.age = a;
     this.rating = rating;
     points = 0;
+  }
+
+  public int compare(Player player1, Player player2){
+    if(player1.points>player2.points){
+      return 1;
+    }
+    else if(player2.points>player1.points){
+      return -1;
+    }
+    else{
+      if(player1.rating>player2.rating){
+        return 1;
+      }
+      else{
+        return -1;
+      }
+    }
   }
 }
