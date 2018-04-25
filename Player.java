72f7021fb00a2e74132c60320547e1e83befaac4
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 15:16:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,10 +2,14 @@ public class Player{
   String name;
   String nationality;
   int age;
+  int rating;
+  int points;
 
-  public Player(String n, String na, int a){
+  public Player(String n, String na, int a, int rating){
     this.name = n;
     this.nationality = na;
     this.age = a;
+    this.rating = rating;
+    points = 0;
   }
 }
