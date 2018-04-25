67eee440b71ab4aafdbaae7b786b90052b4a275d
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 18:12:43 -0400

    Update AddPlayer.java

--- AddPlayer.java
+++ AddPlayer.java
@@ -12,11 +12,13 @@ public class AddPlayer extends JPanel{
     content.setBackground(Color.BLUE);
     content.setLayout(new GridLayout(3,1));
 
+
     Font font = new Font("Verdana", Font.BOLD, 24);
 		//Font reducedFont = new Font("Verdana", Font.BOLD, 24);
 
     JLabel header = new JLabel("ADD PLAYERS!");
     content.add(header);
+
     header.setFont(font);
     header.setForeground(Color.WHITE);
 
@@ -28,52 +30,45 @@ public class AddPlayer extends JPanel{
     JTextField nationality = new JTextField("Add nationality");
     JTextField age = new JTextField("Add age");
     JTextField rating = new JTextField("Add rating");
-    name.setFont(font);
-    nationality.setFont(font);
-    age.setFont(font);
-    rating.setFont(font);
     input.add(name);
     input.add(nationality);
     input.add(age);
     input.add(rating);
 
-    JPanel page_end = new JPanel();
-
     JButton submit = new JButton("Submit");
-    submit.setFont(font);
     submit.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e){
         String n = name.getText();
         String na = nationality.getText();
         String a = age.getText();
         String ra = rating.getText();
-        if(isInteger(a) && isInteger(ra)){
+        if(isInteger(a)&&isInteger(ra)){
           Player p = new Player(n,na,Integer.parseInt(a),Integer.parseInt(ra));
           Competition.players.add(p);
           Competition.playerChooser2.addItem(p);
           Competition.playerChooser.addItem(p);
           Collections.sort(Competition.players);
-          Competition.players.add(p);
-        Competition.playerChooser2.addItem(p);
-        Competition.playerChooser.addItem(p);
-        Collections.sort(Competition.players);
-        Competition.rankingsPanel.removeAll();
-        Competition.rankingsPanel.updateUI();
-        Competition.rankingsPanel.count = 1;
-        for(int i = 0; i<Competition.players.size(); i++){
-          Competition.rankingsPanel.addPlayer(Competition.players.get(i));
-          System.out.println("Hi");
+          Competition.rankingsPanel.removeAll();
+          Competition.rankingsPanel.updateUI();
+          Competition.rankingsPanel.count = 1;
+          for(int i = 0; i<Competition.players.size(); i++){
+            Competition.rankingsPanel.addPlayer(Competition.players.get(i));
+            System.out.println("Hi");
+          }
         }
-        else {
-            warning.showMessageDialog(content,
-            "Failed to submit: Please enter integers",
-            "Inane error",
-            JOptionPane.ERROR_MESSAGE);
+        else{
+          warning.showMessageDialog(content,
+              "Failed to submit: Please enter integers",
+              "Inane error",
+              JOptionPane.ERROR_MESSAGE);
         }
       }
     });
-    page_end.setBackground(Color.BLUE);
+
+    JPanel page_end = new JPanel();
     page_end.add(submit, BorderLayout.CENTER);
+
+    page_end.setBackground(Color.BLUE);
 
     content.add(input);
     content.add(page_end);
@@ -85,6 +80,5 @@ public class AddPlayer extends JPanel{
     }
     return true;
   }
-
 
 }
