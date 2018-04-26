5b3be95b2db0b7f10518bfe0376471948b0af641
lightnesscaster <leinadj24@brandeis.edu>
2018-04-25 22:36:05 -0400

    Update Rankings.java

--- Rankings.java
+++ Rankings.java
@@ -3,15 +3,14 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-//PhoneBookGUI creates a graphical interface that allows the user to input names and phone numbers.
-//Then, the user can search for a the number of a person they know or vice versa.
+//The Rankings class creates a JPanel that displays the relative rankings of
+//various players in a chess tournament
 
 public class Rankings extends JPanel{
 
-  //Use treemaps to store map phone numbers to names, and the reverse
-
   JPanel content;
   JFrame frame;
+  //count the number of players
   int count = 1;
 
 
@@ -20,7 +19,6 @@ public class Rankings extends JPanel{
     //Create a graphical user interface
     super();
     content = this;
-    content.setLayout(new GridBagLayout());
     frame = new JFrame();
     Font font = new Font("Verdana", Font.BOLD, 36);
     content.setFont(font);
@@ -29,16 +27,22 @@ public class Rankings extends JPanel{
     content.setVisible(true);
   }
 
+//This function can be called by the AddPlayer class to add new players to the rankings.
+//It can also be called when a result is inputted via the Competition class to reorder the rankings.
+
   public void addPlayer(Player p){
-    JLabel label = new JLabel(count+".");
+    JLabel label = new JLabel(count+"."); //the number the player is ranked
     count++;
-    JButton nameLabel = new JButton(p.name);
+    JButton nameLabel = new JButton(p.name);//the player's name
+
+    //If the user clicks on the player's name, they get information about that player
     nameLabel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
         String s = "Name: " + p.name + "\n" + "Points: " + p.points + "\n Rating: " + p.rating + "\n Age: " + p.age + "\n Nationality: " + p.nationality;
         JOptionPane.showMessageDialog(frame, s);
       }
     });
+
     content.add(label);
     content.add(nameLabel);
     content.revalidate();
